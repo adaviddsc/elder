49b398a7412654866bbc9339ee9e729d67da5bf3
user <adaviddsc@gmail.com>
2014-10-24 21:13:32 +0800

    elder

---
+++ index.php
@@ -0,0 +1,14 @@
+ <!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="google-site-verification" content="XFLWzNxG-0JQU8yG3B2TlUhqelQSp49V9UaKUZqy-eE" />
+		<meta name="robots" content="index,follow" />
+		<meta name="description" content="喔一顆顆咖咖哭哭你妳哈哈喔喔" />
+		<meta name="keywords" content="喔一顆顆咖咖哭哭你妳哈哈喔喔,成大老老人,成大老老老人" />
+	    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+	</head>
+    <body>
+            <img src="images/日本.jpg" height="780" width="822" alt="">
+    </body>
+</html>
