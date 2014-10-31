633f0617c6b24d05567a0d821d5394c88783b7e7
adaviddsc <adaviddsc@gmail.com>
2014-10-31 23:03:39 +0800

    2014/10/31 elder

--- index.php
+++ index.php
@@ -1,17 +1,76 @@
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8" />
-		<meta name="google-site-verification" content="XFLWzNxG-0JQU8yG3B2TlUhqelQSp49V9UaKUZqy-eE" />
-		<meta name="robots" content="index,follow" />
-		<meta name="description" content="喔一顆顆咖咖哭哭你妳哈哈喔喔" />
-		<meta name="keywords" content="喔一顆顆咖咖哭哭你妳哈哈喔喔,成大老老人,成大老老老人" />
-	    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+    <meta name="google-site-verification" content="XFLWzNxG-0JQU8yG3B2TlUhqelQSp49V9UaKUZqy-eE" />
+    <!--<meta name="robots" content="index,follow" />
+    <meta name="description" content="喔一顆顆咖咖哭哭你妳哈哈喔喔" />
+    <meta name="keywords" content="喔一顆顆咖咖哭哭你妳哈哈喔喔,成大老老人,成大老老老人" />-->
+
+
+    <link rel="stylesheet" href="external/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="external/bootstrap.min.css">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script> 
+    <script type="text/javascript" src="javascripts/index.js"></script>
+    <link rel="stylesheet" href="stylesheets/index.css">
+    <link rel="stylesheet" href="stylesheets/animate.css">
 	</head>
-    <body>
-            <img src="images/日本.jpg" height="780" width="822" alt="">
-            <img src="images/日本.jpg" height="780" width="822" alt="">
-            <img src="images/日本.jpg" height="780" width="822" alt="">
-            <img src="images/日本.jpg" height="780" width="822" alt="">
-    </body>
+  <body>
+    <header>  
+        <h1>後青春的日子</h1>
+        <input type="text" class="form-control title-search" placeholder="search...">
+        <div class="fa fa-search search-button"></div>
+    </header>  
+    <nav>  
+      <ul>
+        <li>帳號管理</li>
+        <div></div>
+        <li>照片管理</li>
+        <div></div>
+        <li>好友管理</li>
+        <div></div>
+        <li>後青春規劃</li>
+        <div></div>
+        <li>醫療規劃</li>
+      </ul>  
+    </nav>
+    <section>
+      <div class="left-nav">
+        <ul>
+          <div class="first-li">最新消息</div>
+          <li>本日推薦活動</li>
+          <li>本日推薦行程</li>
+          <li>本日推薦食物</li>
+          <li>本日推薦音樂</li>
+          <li>本日推薦影集</li>
+        </ul>  
+      </div>
+      <div class="middle-nav">
+        <ul>
+          <div class="first-li">本日心情</div>
+          <li><img src="images/a.jpg" alt=""></li>
+          <li><img src="images/b.jpg" alt=""></li>
+          <li><img src="images/c.jpg" alt=""></li>
+          <li><img src="images/d.jpg" alt=""></li>
+        </ul>  
+      </div>
+      <div class="right-nav">
+        <ul>
+          <div class="first-li">公告欄</div>
+          <li>最新消息:</li>
+          <li>嗨各位同學大家好，今天天氣很好建議各位大哥大姐們應該要多出門走走，避免獨自悶待在家中!</li>
+        </ul>  
+      </div>
+
+    </section>
+    <footer>
+      <p>歡迎參訪我們後青春的日子網站</p>
+      <p>地址 : 台南市東區大學路一號勝利1舍</p>
+      <p>電話 : 06-1234567</p>
+    </footer>
+
+  </body>
+  <script src="external/bootstrap.min.js"></script>
 </html>
